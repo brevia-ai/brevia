94d80ea26dc1ec41464d6f381a63e741d7c25f23
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-10-16 15:25:17 +0200

    fix: remove duplicate online migrations

--- brevia/alembic/env.py
+++ brevia/alembic/env.py
@@ -88,19 +88,6 @@ def run_migrations_online() -> None:
         with context.begin_transaction():
             context.run_migrations()
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
 
 
 if context.is_offline_mode():

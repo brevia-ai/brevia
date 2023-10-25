955514f85a9b06f7329d7ca8e64606fed07a29cc
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-10-25 17:17:27 +0200

    feat: use `FEATURE_QA_LANG_DETECT` flag

--- brevia/language.py
+++ brevia/language.py
@@ -1,4 +1,5 @@
 """Language detection utilities"""
+from os import environ
 import spacy
 from spacy.language import Language
 import langcodes
@@ -6,10 +7,13 @@ import langcodes
 
 class Detector():
     """Language detection class"""
-    nlp: Language
+    nlp: Language | None = None
 
     def __init__(self):
         """Init internal nlp"""
+        if not bool(environ.get('FEATURE_QA_LANG_DETECT')):
+            return
+
         try:
             import spacy_fastlang  # noqa: F401 pylint: disable=import-outside-toplevel
 
@@ -22,7 +26,12 @@ class Detector():
             ) from exc
 
     def detect(self, phrase: str) -> str:
-        """Detect language of generic phrase"""
+        """
+            Detect language of generic phrase, return an empty string
+            if Spacy has not been initialized
+        """
+        if not self.nlp:
+            return ''
         doc = self.nlp(phrase)
 
         return langcodes.Language.make(language=doc._.language).display_name()

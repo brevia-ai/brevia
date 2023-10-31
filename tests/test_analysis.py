9a49254f4385493307d172d1576f0b168ccc3f92
nikazzio <niki.corradetti@gmail.com>
2023-10-31 01:03:47 +0100

    feat: add unit test for analysis prompts

---
+++ tests/test_analysis.py
@@ -0,0 +1,39 @@
+"""Analysis module tests"""
+from brevia.analysis import load_summarize_prompt
+from langchain.prompts import BasePromptTemplate
+
+FAKE_PROMPT = {
+    '_type': 'prompt',
+    'input_variables': [],
+    'template': 'Fake',
+}
+
+
+def test_load_summarize_prompt():
+    """Test load_summarize_prompt method"""
+    result = load_summarize_prompt({
+        'custom': FAKE_PROMPT,
+    })
+    assert result is not None
+    assert isinstance(result, BasePromptTemplate)
+
+    result = load_summarize_prompt({
+        'summarize': FAKE_PROMPT,
+    })
+    assert result is not None
+    assert isinstance(result, BasePromptTemplate)
+
+    result = load_summarize_prompt({
+        'summarize_point': FAKE_PROMPT,
+    })
+    assert result is not None
+    assert isinstance(result, BasePromptTemplate)
+
+    result = load_summarize_prompt({
+        'classification': FAKE_PROMPT,
+    })
+    assert result is not None
+    assert isinstance(result, BasePromptTemplate)
+
+
+

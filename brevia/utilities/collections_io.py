3e3c40026320feee3b37949c4f663d9355c90c9d
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-10-31 19:14:00 +0100

    chore: messages

--- brevia/utilities/collections_io.py
+++ brevia/utilities/collections_io.py
@@ -9,14 +9,14 @@ def export_collection_data(
 ):
     """Export collection data using `psql`"""
     if not collections.collection_name_exists(collection):
-        raise ValueError(f'Collection "{collection}" was not found')
+        raise ValueError(f"Collection '{collection}' was not found")
 
     csv_file_collection = f"{folder_path}/{collection}-collection.csv"
     csv_file_embedding = f"{folder_path}/{collection}-embedding.csv"
     if path.exists(csv_file_collection):
-        raise ValueError(f"CSV file {csv_file_collection} already exists, exiting...")
+        raise ValueError(f"CSV file {csv_file_collection} already exists, exiting")
     if path.exists(csv_file_embedding):
-        raise ValueError(f"CSV file {csv_file_embedding} already exists, exiting...")
+        raise ValueError(f"CSV file {csv_file_embedding} already exists, exiting")
 
     select = f"SELECT * FROM langchain_pg_collection WHERE name = '{collection}'"
     copy = f"\\copy ({select}) TO '{csv_file_collection}' WITH DELIMITER ',' CSV HEADER"
@@ -48,9 +48,9 @@ def import_collection_data(
     csv_file_embedding = f"{folder_path}/{collection}-embedding.csv"
     print(f"Importing from {csv_file_collection} and {csv_file_embedding}")
     if not path.exists(csv_file_collection):
-        raise ValueError(f"CSV file {csv_file_collection} not found, exiting...")
+        raise ValueError(f"CSV file {csv_file_collection} not found, exiting")
     if not path.exists(csv_file_embedding):
-        raise ValueError(f"CSV file {csv_file_embedding} not found, exiting...")
+        raise ValueError(f"CSV file {csv_file_embedding} not found, exiting")
 
     table = 'langchain_pg_collection'
     copy = f"\\copy {table} FROM '{csv_file_collection}' WITH DELIMITER ',' CSV HEADER"

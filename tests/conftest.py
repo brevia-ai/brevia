cdcc7412b772d1838eb07ff9c93053e804575fde
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-11-28 11:50:19 +0100

    test: override settings in unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from alembic import command
 from alembic.config import Config
 from brevia.index import init_index
+from brevia.settings import Settings, get_settings
 
 
 def pytest_sessionstart(session):
@@ -11,10 +12,18 @@ def pytest_sessionstart(session):
     return init_index()
 
 
+def update_settings():
+    """Update settings reading from `tests/.env` file"""
+    load_dotenv(f'{Path(__file__).parent}/.env', override=True)
+    settings = get_settings()
+    new_settings = Settings(_env_file=['.env', f'{Path(__file__).parent}/.env'])
+    settings.update(new_settings)
+
+
 @pytest.fixture(autouse=True)
 def env_vars_db():
     """Load .env inside `tests` folder"""
-    load_dotenv(f'{Path(__file__).parent}/.env', override=True)
+    update_settings()
     root_path = Path(__file__).parent.parent
     alembic_conf = Config(f'{root_path}/alembic.ini')
     command.upgrade(alembic_conf, 'head')

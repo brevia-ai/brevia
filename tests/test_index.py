3dc9626fe98b6dd1999afa2c949e4d1b19efca94
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-03-27 19:45:37 +0100

    test: update tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,8 +1,12 @@
 """Index module tests"""
 from pathlib import Path
 import pytest
-from brevia.index import load_pdf_file
+from brevia.index import (
+    load_pdf_file, update_links_documents,
+    add_document, document_has_changed, select_load_link_options
+)
 from brevia.collections import create_collection
+from langchain.docstore.document import Document
 
 
 def test_load_pdf_file():
@@ -19,3 +23,46 @@ def test_load_pdf_file_fail():
     with pytest.raises(FileNotFoundError) as exc:
         load_pdf_file(file_path=file_path, collection_name='test')
     assert str(exc.value) == file_path
+
+
+def test_update_links_documents():
+    """Test update_links_documents method with zero results"""
+    result = update_links_documents('test')
+    assert result == 0
+
+    create_collection('test', {})
+    doc1 = Document(page_content='some', metadata={'type': 'links'})
+    add_document(document=doc1, collection_name='test')
+    result = update_links_documents('test')
+    assert result == 0
+
+
+def test_document_has_changed():
+    """Test document_has_changed method"""
+    collection = create_collection('test', {})
+
+    doc1 = Document(page_content='some', metadata={})
+    add_document(document=doc1, collection_name='test', document_id='1')
+    result = document_has_changed(doc1, collection.uuid, '1')
+    assert result is False
+
+    doc1.metadata = {'type': 'links'}
+    result = document_has_changed(doc1, collection.uuid, '1')
+    assert result is True
+
+    add_document(document=Document(page_content='another'), collection_name='test', document_id='1')
+    result = document_has_changed(doc1, collection.uuid, '1')
+    assert result is True
+
+
+def test_select_load_link_options():
+    """Test select_load_link_options method"""
+    options = [{'url': 'example.com', 'selector': 'test'}]
+    result = select_load_link_options(url='example.com', options=options)
+    assert result == {'selector': 'test'}
+
+    result = select_load_link_options(url='example.com/somepath', options=options)
+    assert result == {'selector': 'test'}
+
+    result = select_load_link_options(url='someurl.org', options=options)
+    assert result == {}

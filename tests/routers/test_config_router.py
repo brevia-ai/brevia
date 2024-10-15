0bfde2db45ed7ebd2ef6e9d82c4f7d4085d0af8a
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-10-15 12:17:39 +0200

    test: update router cases

--- tests/routers/test_config_router.py
+++ tests/routers/test_config_router.py
@@ -3,7 +3,7 @@ from json import dumps
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 from brevia.routers import config_router
-from brevia.settings import configurable_settings
+from brevia.settings import configurable_settings, get_settings
 from tests import conftest
 
 app = FastAPI()
@@ -18,16 +18,19 @@ def test_get_config():
     assert response.json() == {}
 
 
-def test_get_config_keys():
-    """Test /config/keys failure"""
-    response = client.get('/config/keys', headers={})
+def test_get_config_schema():
+    """Test /config/schema endpoint"""
+    response = client.get('/config/schema', headers={})
 
     assert response.status_code == 200
-    assert response.json() == configurable_settings()
+    schema = response.json()
+    assert 'properties' in schema
+    assert all(key in schema['properties'] for key in configurable_settings())
 
 
 def test_post_config():
     """Test POST /config endpoint"""
+    current = get_settings().search_docs_num
     response = client.post(
         '/config',
         headers={'Content-Type': 'application/json'},
@@ -37,13 +40,15 @@ def test_post_config():
     )
     # update test settings since get_settings.cache_clear() was called
     conftest.update_settings()
+    assert get_settings().search_docs_num == 8
+    get_settings().search_docs_num = current
 
     assert response.status_code == 200
     assert response.json() == {'search_docs_num': '8'}
 
 
-def test_post_config_failure():
-    """Test POST /config endpoint"""
+def test_post_config_extra_failure():
+    """Test POST /config endpoint failure"""
     response = client.post(
         '/config',
         headers={'Content-Type': 'application/json'},
@@ -55,4 +60,25 @@ def test_post_config_failure():
     conftest.update_settings()
 
     assert response.status_code == 400
-    assert response.json() == {'detail': 'Setting "test_key" is not configurable'}
+    assert response.json() == {
+        'detail': 'There are not configurable settings: test_key'
+    }
+
+
+def test_post_config_invalid_failure():
+    """Test POST /config endpoint failure"""
+    response = client.post(
+        '/config',
+        headers={'Content-Type': 'application/json'},
+        content=dumps({
+            'search_docs_num': 'gustavo',
+        })
+    )
+    # update test settings since get_settings.cache_clear() was called
+    conftest.update_settings()
+
+    assert response.status_code == 400
+    assert 'detail' in response.json()
+    detail = response.json()['detail']
+    assert 'search_docs_num' in detail
+    assert 'gustavo' in detail

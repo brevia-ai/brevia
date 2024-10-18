b9d75adbcc7d8433a445d8d179b6e2efb9abf626
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-10-18 17:06:09 +0200

    feat: dynamically load any LLM/Embeddings model

--- brevia/utilities/types.py
+++ brevia/utilities/types.py
@@ -2,10 +2,18 @@
 from importlib import import_module
 
 
-def load_type(name: str, parent_type: type | None = None) -> type:
+def load_type(name: str, parent_type: type | None = None, default_module:
+              str | None = None) -> type:
     """ Return a type from a string with a python module path """
+    if '.' not in name:
+        if not default_module:
+            raise ValueError('default_module is needed for name without a module path')
+        name = f'{default_module}.{name}'
     module_name, class_name = name.rsplit('.', 1)
-    module = import_module(module_name)
+    try:
+        module = import_module(module_name)
+    except ModuleNotFoundError:
+        raise ValueError(f'Module "{module_name}" not found')
     if not hasattr(module, class_name):
         raise ValueError(f'Class "{class_name}" not found in "{module}"')
     new_class = getattr(module, class_name)

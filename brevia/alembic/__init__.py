8c4e3c0d7af952a0596ea90644b41c2fe4a85c02
stefanorosanelli <stefano.rosanelli@gmail.com>
2024-12-19 16:11:20 +0100

    fix: set alembic `script_location` config at runtime

--- brevia/alembic/__init__.py
+++ brevia/alembic/__init__.py
@@ -1,20 +1,20 @@
 """DB migrations commands """
 from os import getcwd
-
+from os.path import dirname
 from alembic.config import Config
 from alembic import command
 
-
-ALEMBIC_CFG = Config(f'{getcwd()}/alembic.ini')
+alembic_cfg = Config(f'{getcwd()}/alembic.ini')
+alembic_cfg.set_main_option('script_location', dirname(__file__))
 
 
 def current(verbose=False):
-    command.current(ALEMBIC_CFG, verbose=verbose)
+    command.current(alembic_cfg, verbose=verbose)
 
 
 def upgrade(revision="head"):
-    command.upgrade(ALEMBIC_CFG, revision)
+    command.upgrade(alembic_cfg, revision)
 
 
 def downgrade(revision):
-    command.downgrade(ALEMBIC_CFG, revision)
+    command.downgrade(alembic_cfg, revision)

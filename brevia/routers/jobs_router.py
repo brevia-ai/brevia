92bbf545e1a03f5865c78c0ec527ac8fdc3a12f9
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2025-07-01 18:29:34 +0200

    feat: async jobs filtering and pagination with new endpoint

--- brevia/routers/jobs_router.py
+++ brevia/routers/jobs_router.py
@@ -1,7 +1,8 @@
 """API endpoints definitions to handle async jobs"""
-from fastapi import APIRouter, HTTPException, status
+from typing_extensions import Annotated
+from fastapi import APIRouter, HTTPException, status, Depends
 from brevia.dependencies import get_dependencies
-from brevia import async_jobs
+from brevia.async_jobs import get_jobs, single_job, JobsFilter
 
 router = APIRouter()
 
@@ -9,13 +10,13 @@ router = APIRouter()
 @router.get(
     '/jobs/{uuid}',
     dependencies=get_dependencies(json_content_type=False),
-    tags=['Analysis'],
+    tags=['Analysis', 'Jobs'],
 )
 async def read_analysis_job(uuid: str):
     """
     Read details of a single analisys Job via its UUID
     """
-    job = async_jobs.single_job(uuid)
+    job = single_job(uuid)
     if job is None:
         raise HTTPException(
             status.HTTP_404_NOT_FOUND,
@@ -23,3 +24,13 @@ async def read_analysis_job(uuid: str):
         )
 
     return job
+
+
+@router.get(
+    '/jobs',
+    dependencies=get_dependencies(json_content_type=False),
+    tags=['Jobs'],
+)
+async def list_analysis_jobs(filter: Annotated[JobsFilter, Depends()]):
+    """ /jobs endpoint, list all analysis jobs """
+    return get_jobs(filter=filter)
